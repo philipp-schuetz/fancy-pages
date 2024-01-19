95f0c85f4d9da7c770c35c6b8b40c9072d7469f2
philipp-schuetz <mail@philippschuetz.com>
2024-01-19 14:27:37 +0100

    Add solutions

--- 03_calc-u-later/main.js
+++ 03_calc-u-later/main.js
@@ -14,12 +14,54 @@ document.getElementById('reset').addEventListener("click", function() { reset();
 function pushToOutput(value) {
     document.getElementById("output").innerText += value;
 }
+function expr (expr) {
+
+    var chars = expr.split("");
+    var n = [], op = [], index = 0, oplast = true;
+
+    n[index] = "";
+
+    // Parse the expression
+    for (var c = 0; c < chars.length; c++) {
+
+        if (isNaN(parseInt(chars[c])) && chars[c] !== "." && !oplast) {
+            op[index] = chars[c];
+            index++;
+            n[index] = "";
+            oplast = true;
+        } else {
+            n[index] += chars[c];
+            oplast = false;
+        }
+    }
+    // Calculate the expression
+    expr = parseFloat(n[0]);
+    for (var o = 0; o < op.length; o++) {
+        var num = parseFloat(n[o + 1]);
+        switch (op[o]) {
+            case "+":
+                expr = expr + num;
+                break;
+            case "-":
+                expr = expr - num;
+                break;
+            case "*":
+                expr = expr * num;
+                break;
+            case "/":
+                expr = expr / num;
+                break;
+        }
+    }
+    return expr;
+}
 
 function calculate() {
     var output = document.getElementById("output");
-    output.innerText = "Not Implemented";
+    output.innerText = expr(output.innerText);
 }
 
 function reset() {
     document.getElementById("output").innerText = "";
 }
+
